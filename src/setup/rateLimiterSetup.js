936b2ccf69636592ba8eac0bd7412c8d63b08526
AlejandroEspinosa <116526021+AL3J4NDRO17@users.noreply.github.com>
2025-03-27 03:05:03 -0600

    Update rateLimiterSetup.js

--- src/setup/rateLimiterSetup.js
+++ src/setup/rateLimiterSetup.js
@@ -1,8 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 1000, // Máximo 100 solicitudes por IP
+  windowMs: 1 * 60 * 1000, // 15 minutos
+  max: 1000000000, // Máximo 100 solicitudes por IP
   message: "Demasiadas solicitudes, intenta de nuevo más tarde.",
 });
 
